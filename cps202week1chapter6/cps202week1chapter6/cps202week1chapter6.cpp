26c7ca104f7c3c3ee4377714597f7da177f6b67e
phelpsinc <chuck@phelpsinc.net>
2017-06-08 18:42:08 -0500

    Week 6 Chapter 6

---
+++ cps202week1chapter6/cps202week1chapter6/cps202week1chapter6.cpp
@@ -0,0 +1,30 @@
+// cps202week1chapter6.cpp : Defines the entry point for the console application.
+//============================================================
+// Name: Chuck Phelps
+// Created: 06/08/2017
+// Updated: 06/08/2017
+// Purpose:
+//=============================================================
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+void main()
+{
+    
+	
+	
+	
+	
+	system("pause");
+	return;
+}
+
+void dispSomething()
+{
+	cout << "display someting" << endl;
+}
+
